37a57987c98a2ec2894f03a0ef0a5c51650c015b
bomibear <bomibearly@gmail.com>
2019-06-26 14:14:39 -0700

    finish style for highscore board

--- js/HighScoreBoard.js
+++ js/HighScoreBoard.js
@@ -2,12 +2,9 @@
 
 var scoreList = document.getElementById('hightScore');
 var arrayOfTop5 = JSON.parse(localStorage.getItem('Top5'));
+
 for(var i = 0; i < arrayOfTop5.length; i++){
   var listEl = document.createElement('li');
-  listEl.textContent = arrayOfTop5[i].name + ' ' + arrayOfTop5[i].score;
+  listEl.textContent = arrayOfTop5[i].name + ' : ' + arrayOfTop5[i].score;
   scoreList.appendChild(listEl);
-
 }
-
-
-
